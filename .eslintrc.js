c64988ec56e0e0f1b685befa990203a73707c32c
TDomagalski <tomekwebdev@gmail.com>
2023-05-18 17:22:36 +0200

    feat(prettier): add and configuration prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,27 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	extends: ['airbnb'],
-	overrides: [],
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: ['react'],
-	rules: {
-		'react/react-in-jsx-scope': 'off',
-		'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-		'react/jsx-indent': 'off',
-		'react/jsx-indent-props': 'off',
-		'react/jsx-one-expression-per-line': 'off',
-		'react/prop-types': 'off',
-		indent: [2, 'tab'],
-		'no-tabs': 0,
-	},
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'plugin:prettier/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'jsx-a11y'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-indent': 'off',
+    'react/jsx-indent-props': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/prop-types': 'off',
+    indent: [3, 'tab'],
+    'no-tabs': 0,
+  },
 };
